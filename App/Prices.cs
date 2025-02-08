07e840e85936ccab35a4a72afc512d4d2853e887
mashasha <tok.masha.dm@yandex.ru>
2025-02-08 17:34:14 +0700

    first commit

--- App/Prices.cs
+++ App/Prices.cs
@@ -1,10 +1,43 @@
+using System.Diagnostics;
+
 namespace App;
 
 public static class Prices
 {
-    public static string GetCurrencyAlias(int price, bool isShorNotation, bool isFirstCapital)
+
+    public static string GetCurrencyAlias(int price, bool isShortNotation, bool isFirstCapital)
     {
-        //TODO напишите тут свое решение
-        throw new NotImplementedException();
+        Debug.Assert(price >= 0);
+        if (isShortNotation)
+        {
+            return isFirstCapital ? "Руб." : "руб.";
+        }
+
+        if (price / 10 % 10 == 1)
+        {
+            return isFirstCapital ? "Рублей" : "рублей";
+        }
+
+        switch (price % 10)
+        {
+            case 0:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                return isFirstCapital ? "Рублей" : "рублей";
+            case 1:
+                return isFirstCapital ? "Рубль" : "рубль";
+
+            case 2:
+            case 3:
+            case 4:
+                return isFirstCapital ? "Рубля" : "рубля";
+
+        }
+
+        return "oh no";
+
     }
 }
--- App/Program.cs
+++ App/Program.cs
@@ -2,8 +2,16 @@
 
 public static class Program
 {
+    static bool IsDigit(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        for (int i = 0; i < 200; i++)
+        {
+            Console.WriteLine($"{i} : {Prices.GetCurrencyAlias(i, false, false)}");
+        }
+        
     }
 }
